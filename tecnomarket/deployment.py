6d0eb01d3e9681f9c1ce429d5d7f7dd06a377ea6
Zombermanic <leocacru@gmail.com>
2024-11-30 21:19:28 -0300

    cambios en deployment.py

--- tecnomarket/deployment.py
+++ tecnomarket/deployment.py
@@ -1,12 +1,22 @@
 import os
 from .settings import BASE_DIR
 
+# Clave secreta obtenida desde una variable de entorno
 SECRET_KEY = os.environ['SECRET']
-print(os.environ.get('WEBSITE_HOSTNAME'))
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]  # Verifica que esta variable esté configurada correctamente
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]  # Verifica que la URL esté bien configurada
+
+# Permitir el dominio configurado en Azure
+WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME', '')
+ALLOWED_HOSTS = [WEBSITE_HOSTNAME] if WEBSITE_HOSTNAME else []
+
+# Proteger el origen de las solicitudes CSRF
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{WEBSITE_HOSTNAME}"
+] if WEBSITE_HOSTNAME else []
+
+# Modo de depuración desactivado en producción
 DEBUG = False
 
+# Middleware configurado correctamente
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -18,18 +28,20 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Configuración de archivos estáticos
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
+# Configuración de la base de datos desde variables de entorno
+connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING', '')
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')} if connection_string else {}
 
-DATABASE = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Corrige el error de 'EGINE' a 'ENGINE'
-        'NAME': parameters['dbname'],
-        'HOST': parameters['host'],
-        'USER': parameters['user'],
-        'PASSWORD': parameters['password'],
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters.get('dbname', ''),
+        'HOST': parameters.get('host', ''),
+        'USER': parameters.get('user', ''),
+        'PASSWORD': parameters.get('password', ''),
     }
 }
