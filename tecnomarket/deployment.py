667aea992b88cc2b61ed518a3bd1b19fe3de21ff
Zombermanic <leocacru@gmail.com>
2024-11-30 21:01:17 -0300

    cambios deployment.py

--- tecnomarket/deployment.py
+++ tecnomarket/deployment.py
@@ -1,16 +1,14 @@
 import os
-from settings.py import *
 from .settings import BASE_DIR
 
-
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]  # Verifica que esta variable esté configurada correctamente
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]  # Verifica que la URL esté bien configurada
 DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware ',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -22,13 +20,13 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['AZURE_POSTGERSQL_CONNECTIONSTRING']
-parameters= {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 
-DATABASE ={
-    'default':{
-        'EGINE': 'django.db.backends.postgersql',
-        'NAME' : parameters['dbname'],
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # Corrige el error de 'EGINE' a 'ENGINE'
+        'NAME': parameters['dbname'],
         'HOST': parameters['host'],
         'USER': parameters['user'],
         'PASSWORD': parameters['password'],
